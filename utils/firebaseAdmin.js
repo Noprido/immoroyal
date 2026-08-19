5cc89e0099c2fe9458e61a9fa71e86c5bd2b1044
Noprido <104641003+Noprido@users.noreply.github.com>
2026-08-19 23:55:37 +0100

    Add files via upload

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,6 +1,15 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getMessaging } = require('firebase-admin/messaging');
-const serviceAccount = require('../config/firebase-service-account.json');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Sur Render (ou tout autre hébergeur) : lu depuis la variable d'environnement
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // En local sur ta machine : lu depuis le fichier (jamais commité)
+  serviceAccount = require('../config/firebase-service-account.json');
+}
 
 const app = initializeApp({
   credential: cert(serviceAccount),
